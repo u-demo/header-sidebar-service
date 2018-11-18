4f10933713b5df5dfd2b2ec28585ae5758d69d5a
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-11-17 17:19:39 -0800

    Editseed file to generate 10M data of courses

--- Db/seed.js
+++ Db/seed.js
@@ -1,13 +1,13 @@
 //const db = require('.index.js');
-const faker = require('faker');
+const { hacker, name } = require('faker');
 const path = require('path');
-const csv  = require('fast-csv');
+const csv = require('fast-csv');
 const fs = require('fs');
 
 const ccOptions = ['English', 'Spanish', 'Mandarin', 'Korean', 'Japanese', 'German', 'Russian', 'French', 'Italian'];
 const tagLabel = ['Best Seller', 'Highest Rated', 'Hot & New', 'New', null];
 const languages = ['English', 'English, Spanish', 'English, Mandarin', 'English, Korean', 'English, Japanese'];
-const descriptionPrefix =[
+const descriptionPrefix = [
   'A complete guide for ',
   'An adventure in ',
   'Learn professional ',
@@ -19,12 +19,45 @@ const descriptionPrefix =[
   'Get an in-depth understanding of core skills & advanced in ',
   'Master design principles, best practices and coding conventions for ',
 ]
-let temp = [];
+const temp = [];
 
 const getRandomNum = max => Math.floor(Math.random() * Math.floor(max));
 
 const getRandomNumInRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
+const generateNewCourse = function generateNewCourse(i) {
+  const desc = `${descriptionPrefix[getRandomNum(descriptionPrefix.length)]}${hacker.ingverb()} ${hacker.adjective()} ${hacker.abbreviation()}`;
+  const enrollmentTotal = getRandomNumInRange(15, 500);
+  const ratingsTotal = getRandomNumInRange(10, enrollmentTotal);
+  const listPrice = getRandomNumInRange(50, 300);
+  const discPrice = Math.floor(listPrice * (getRandomNumInRange(10, 20) * 0.01));
+  const yearCreated = getRandomNumInRange(5, 9);
+  const monthCreated = getRandomNumInRange(1, 12);
+  const yearUpdated = getRandomNumInRange(yearCreated, 9);
+  const monthUpdated = getRandomNumInRange(monthCreated, 12);
+  const newCourse = {
+    id: i,
+    title: temp[i],
+    description: desc,
+    tag: tagLabel[getRandomNum(tagLabel.length)],
+    rating: Number((Math.random() * (5 - 3.4) + 3.4).toFixed(1)),
+    count_ratings: ratingsTotal,
+    enrollment: enrollmentTotal,
+    created_by: name.findName(),
+    created_at: `${monthCreated}/201${yearCreated}`,
+    updated_at: `${monthUpdated}/201${yearUpdated}`,
+    language: languages[getRandomNum(languages.length)],
+    img_url: `https://s3.amazonaws.com/header-sidebar-service/course${getRandomNum(111)}.jpg`,
+    list_price: `$${listPrice}.99`,
+    discount_price: `$${discPrice}.99`,
+    video_hrs: Number((Math.random() * (30 - 12) + 12).toFixed(1)),
+    total_articles: getRandomNumInRange(1, 15),
+    total_downloads: getRandomNumInRange(3, 15),
+    active_coupon: 'WEDEMO',
+    cc_options: ccOptions[getRandomNum(ccOptions.length)],
+  };
+  return newCourse;
+};
 
 // Format
 const csvStreamCourses = csv.format({ headers: true }).transform(row => ({
@@ -37,7 +70,7 @@ const csvStreamCourses = csv.format({ headers: true }).transform(row => ({
   enrollment: row.enrollment,
   created_by: row.created_by,
   created_at: row.created_at,
-  last_updated_at: row.last_updated_at,
+  updated_at: row.updated_at,
   language: row.language,
   img_url: row.img_url,
   list_price: row.list_price,
@@ -46,47 +79,32 @@ const csvStreamCourses = csv.format({ headers: true }).transform(row => ({
   total_articles: row.total_articles,
   total_downloads: row.total_downloads,
   active_coupon: row.active_coupon,
-  ccOptions: row.ccOptions,
+  cc_options: row.cc_options,
 }));
 
 const writableStream = fs.createWriteStream('./courses.csv');
 csvStreamCourses.pipe(writableStream);
-
-
-function createCourseData() {
+const numData = 10000000;
+let i = 0;
+function createCourseData(callback) {
   // For each title in temp array,
   // Write other fake datas needed for header and sidebar
-  for (let i = 0; i < 5; i += 1) {
-    const desc = descriptionPrefix[Math.floor(Math.random() * descriptionPrefix.length)] + faker.hacker.noun;
-    const enrollmentTotal = getRandomNumInRange(15, 105);
-    const ratingsTotal = getRandomNumInRange(10, enrollmentTotal);
-    const randomIdx = getRandomNum(tagLabel.length);
-    const listPrice = getRandomNumInRange(50, 300);
-    const discPrice = Math.floor(listPrice * (getRandomNumInRange(10, 20) * 0.01));
-    let newCourse = {
-      id: i + 1,
-      title: temp[i],
-      description: desc,
-      tag: tagLabel[Math.floor(Math.random() * 5)],
-      rating: Number((Math.random() * (5 - 3.4) + 3.4).toFixed(1)),
-      count_ratings: ratingsTotal,
-      enrollment: enrollmentTotal,
-      created_by: faker.name.findName(),
-      created_at: Date.now(),
-      last_updated_at: `${getRandomNumInRange(1, 29)}/201${getRandomNumInRange(5, 9)}`,
-      language: languages[randomIdx],
-      img_url: 'https://randomuser.me/api/portraits/',
-      list_price: listPrice,
-      discount_price: `$${discPrice}.99`,
-      video_hrs: Number((Math.random() * (30 - 12) + 12).toFixed(1)),
-      total_articles: getRandomNumInRange(1, 15),
-      total_downloads: getRandomNumInRange(3, 15),
-      active_coupon: 'ILOVEUDEMO',
-      ccOptions: ccOptions[getRandomNum(9)],
-    };
-    csvStreamCourses.write(newCourse);
+  let memoryAllows = true;
+  while (i <= numData && memoryAllows) {
+    i += 1;
+    const newCourse = generateNewCourse(i);
+    if (i === numData) {
+      csvStreamCourses.write(newCourse, callback);
+    } else {
+      memoryAllows = csvStreamCourses.write(newCourse);
+    }
   }
-  csvStreamCourses.end();
+  if (i < numData && !memoryAllows) {
+    writableStream.once('drain', createCourseData);
+  } else {
+    console.log(`WRITING ${numData} data to CSV DONE!`);
+    csvStreamCourses.end();
+  }
 }
 
 // Read CSV file with course title build temp array for title
@@ -97,9 +115,30 @@ csv
     temp.push(data[1]);
   })
   .on('end', () => {
-    createCourseData();
+    createCourseData(writeToDB);
   });
 
+const writeToDB = function writeToDB() {
+  console.log('WRITING TO DB!');
+};
+
+
+// csv
+//   .fromPath(path.join(__dirname, '/courses.csv'))
+//   .on('data', (newData) => {
+//     console.log(newData);
+//   })
+
+// Read CSV file with course title build temp array for title
+// csv
+//   .fromPath(path.join(__dirname, '/courses.csv'))
+//   .on('data', (data) => {
+//     console.log(data);
+    
+//   })
+//   .on('end', () => {
+//     console.log('hi');
+//   });
   // .on(() => {
     
   //   //if(error) console.log(error);
@@ -119,4 +158,5 @@ csv
 //   console.log(err ? err.stack : res.rows[0].message) // Hello World!
 //   client.end();
 //   });
+
 
