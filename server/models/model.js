4bf12cbf8489bac1b0d82c206c2962954213f007
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-11-17 17:22:19 -0800

    Start edit to postgres

--- server/models/model.js
+++ server/models/model.js
@@ -3,19 +3,20 @@ const Promise = require('bluebird');
 require('dotenv').config();
 
 // use below to run locally
-// const connection = mysql.createConnection({
-//   user: 'root',
-//   database: 'headerSidebar',
-// });
+const connection = mysql.createConnection({
+  user: 'root',
+  password: 'student',
+  database: 'headerSidebar',
+});
 
 // use below to run from aws
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-});
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_DATABASE,
+//   port: process.env.DB_PORT,
+// });
 
 const db = Promise.promisifyAll(connection, { multiArgs: true });
 
