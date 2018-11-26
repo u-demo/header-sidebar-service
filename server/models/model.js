a12e29e3ab4b163ae9e84c76b097b1471a5e5489
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-11-26 15:37:56 -0800

    Delete commemts

--- server/models/model.js
+++ server/models/model.js
@@ -16,15 +16,3 @@ const config = {
 const db = pgp(config);
 
 module.exports = db;
-
-// use below to run from aws
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_DATABASE,
-//   port: process.env.DB_PORT,
-// });
-
-// const db = Promise.promisifyAll(connection, { multiArgs: true });
-
